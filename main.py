95cbf0a96fc168c64249136f809a22a1541861b5
Taoheed_O <oyeniyiemperor@gmail.com>
2023-12-31 00:27:06 +0100

    code update

--- main.py
+++ main.py
@@ -276,33 +276,3 @@ elif choose=="Info":
 
     pass
 
-
-
-
-# Lower sidebar
-
-
-# Image code with encoder ---  not needed here
-# # Set the background image using a local image
-# # You need to encode the image as base64 first
-# import base64
-# def get_base64_of_bin_file(bin_file):
-#     with open(bin_file, 'rb') as f:
-#         data = f.read()
-#     return base64.b64encode(data).decode()
-
-# def set_png_as_page_bg(png_file):
-#     bin_str = get_base64_of_bin_file(png_file)
-#     page_bg_img = '''
-#     <style>
-#     body {
-#     background-image: url("data:image/png;base64,%s");
-#     background-size: cover;
-#     }
-#     </style>
-#     ''' % bin_str
-    
-#     st.markdown(page_bg_img, unsafe_allow_html=True)
-
-# # Use the function
-# set_png_as_page_bg('images/artificial-intelligence.jpg')
